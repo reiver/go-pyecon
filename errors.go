e72158c4d45b263755c6554b3a6ff47ffb1e6094
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-06-17 05:31:33 -0700

    errors

--- errors.go
+++ errors.go
@@ -8,6 +8,7 @@ const (
 	errMissingContractAddress = erorr.Error("pyecon: missing contract-address")
 	errMissingRPCURL          = erorr.Error("pyecon: missing rpc-url")
 	errNilCallData            = erorr.Error("pyecon: nil call-datao")
+	errNilLog                 = erorr.Error("pyecon: nil log")
 	errNilReceiver            = erorr.Error("pyecon: nil receiver")
 	errNilRPCClient           = erorr.Error("pyecon: nil rpc-client")
 )
