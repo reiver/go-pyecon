8b16f720f6dba0bb405b649c58dc1cc8cafb7502
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-06-17 12:35:40 -0700

    pyecon.Client.CurrentBlockNumber()

---
+++ client_currentblocknumber.go
@@ -0,0 +1,55 @@
+package pyecon
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	ethrpc "github.com/ethereum/go-ethereum/rpc"
+
+	"sourcecode.social/reiver/go-erorr"
+)
+
+// CurrentBlockNumber returns the current (newest) block-number for the blockchain-network which this client is conencted to.
+//
+// Each blockchain-network will (likely) have a different current (newest) blockchain-number.
+//
+// And each blockchain-network's current (newest) block-number will change over time.
+//
+// Example:
+//
+//	currentBlockNumber, err := client.CurrentBlockNumber()
+func (receiver Client) CurrentBlockNumber() (*big.Int, error) {
+
+	var rpcurl string
+	{
+		var something bool
+
+		rpcurl, something = receiver.rpcurl.Get()
+		if !something {
+			return nil, errMissingRPCURL
+		}
+	}
+
+	var client *ethclient.Client
+	{
+		rpcclient, err := ethrpc.Dial(rpcurl)
+		if err != nil {
+			return nil, erorr.Errorf("pyecon: problem creating RPC client: %s", err)
+		}
+		defer rpcclient.Close()
+
+		client = ethclient.NewClient(rpcclient)
+		if nil == client {
+			return nil, errNilRPCClient
+		}
+		defer client.Close()
+	}
+
+	header, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		return nil, erorr.Errorf("pyecon: problem getting the current (latest) block-number: %s", err)
+	}
+
+	return header.Number, nil
+}
