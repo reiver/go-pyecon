eec39084928a0559bd890777299298108c039ed5
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-06-19 06:29:12 -0700

    maturity

--- errors.go
+++ errors.go
@@ -5,10 +5,12 @@ import (
 )
 
 const (
+	errMaturityOverflow       = erorr.Error("pyecon: 'maturity' overflow")
 	errMissingContractAddress = erorr.Error("pyecon: missing contract-address")
 	errMissingRPCURL          = erorr.Error("pyecon: missing rpc-url")
 	errNilCallData            = erorr.Error("pyecon: nil call-datao")
 	errNilLog                 = erorr.Error("pyecon: nil log")
+	errNilMaturity            = erorr.Error("pyecon: nil 'maturity'")
 	errNilReceiver            = erorr.Error("pyecon: nil receiver")
 	errNilRPCClient           = erorr.Error("pyecon: nil rpc-client")
 )
